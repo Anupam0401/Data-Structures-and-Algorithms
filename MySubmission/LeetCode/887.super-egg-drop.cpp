2b27bda75dfad8fefae06285148ef098a50d3038
Anupam <anupamk@iitbhilai.ac.in>
2022-05-26 23:44:10 +0530

    add the most optimized code for eggDrop problem

--- MySubmission/LeetCode/887.super-egg-drop.cpp
+++ MySubmission/LeetCode/887.super-egg-drop.cpp
@@ -49,7 +49,8 @@ public:
         // return dp[n][k]+1;
         vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
         int m = 0;
-        while (dp[m][K] < N) {
+        while (dp[m][K] < N)
+        {
             m++;
             for (int k = 1; k <= K; ++k)
                 dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1;
