4730a254f8cd3ad17b3e63de10e7e3891982f48c
Anupam <anupamk@iitbhilai.ac.in>
2022-05-28 20:50:18 +0530

    leetcode q2

--- MySubmission/test.cpp
+++ MySubmission/test.cpp
@@ -1,74 +1,40 @@
-// Given a string S on which you need to perform Q replace operations.
-// Each replacement operation has 3 parameters: a starting index i, a source word x and a target word y. The rule is that if x starts at position i in the original string S, then we will replace that occurrence of x with y. If not, we do nothing.
+// You have a chat log of n messages. You are given two string arrays messages and senders where messages[i] is a message sent by senders[i].
 
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
+// A message is list of words that are separated by a single space with no leading or trailing spaces. The word count of a sender is the total number of words sent by the sender. Note that a sender may send more than one message.
+
+// Return the sender with the largest word count. If there is more than one sender with the largest word count, return the one with the lexicographically largest name.
+
+// Note:
+
+// - Uppercase letters come before lowercase letters in lexicographical order.
+// - "Alice" and "alice" are distinct.
 
 
- // } Driver Code Ends
-//User function Template for C++
-
-// class Solution {
-//   public:
-//     string findAndReplace(string S ,int Q, int index[], string sources[], string targets[]) {
-//         for(int i=0;i<Q;i++){
-//             if(sources[i]==S.substr(index[i],index[i]+sources[i].length())){
-//                 S=S.substr(0,index[i]-1)+S.substr(index[i]+sources[i].length()+1);
-//                 S.insert(index[i],sources[i]);
-//             }
-//         }
-//         return S;
-//     }
-// };
-class Solution
-{
+class Solution {
 public:
-    string findAndReplace(string S, int Q, int index[], string sources[], string targets[])
-    {
-        int addIndex = 0;
-        for (int i = 0; i < Q; i++)
-        {
-            int start = index[i]+addIndex;
-            string source = sources[i];
-            string target = targets[i];
-            int len = source.length();
-            cout<<start<<" "<<len<<endl;
-            if (S.substr(start, len) == source)
-            {
-                cout<<"IN"<<endl;
-                S.replace(start, len, target);
-                addIndex+=target.length()-len;
+    int wordSize(string sentence){
+        //find the number of words in the sentence
+        int count = 0;
+        for(int i = 0; i < sentence.size(); i++){
+            if(sentence[i] == ' '){
+                count++;
             }
         }
-        return S;
+        return count + 1;
+    }
+    string largestWordCount(vector<string>& messages, vector<string>& senders) {
+        unordered_map<string, int> m;
+        for (int i = 0; i < messages.size(); ++i) {
+            m[senders[i]] += wordSize(messages[i]);
+        }
+        string res;
+        int max_cnt = 0;
+        for (auto& p : m) {
+            if (p.second > max_cnt || (p.second == max_cnt && p.first > res)) {
+                max_cnt = p.second;
+                res = p.first;
+            }
+        }
+        return res;
     }
 };
-
-
-// { Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string S;
-        cin>>S;
-        
-        int Q;
-        cin>>Q;
-        int index[Q];
-        string sources[Q], targets[Q];
-        
-        for(int i=0; i<Q; i++)
-            cin>>index[i];
-        for(int i=0; i<Q; i++)
-            cin>>sources[i];
-        for(int i=0; i<Q; i++)
-            cin>>targets[i];
-
-        Solution ob;
-        cout<<ob.findAndReplace(S,Q,index,sources,targets)<<endl;
-    }
-    return 0;
-}  // } Driver Code Ends
