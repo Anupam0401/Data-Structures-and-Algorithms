5f0b30d4a5e294e466fd24484d7054ee941e287e
Anupam <anupamk@iitbhilai.ac.in>
2022-06-04 23:37:16 +0530

    implemented bactracking soln with hash table : most optimal

---
+++ MySubmission/LeetCode/51.n-queens.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=51 lang=cpp
+ *
+ * [51] N-Queens
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void solve(int col,int n,vector<vector<string>> &ans, vector< string> &board,vector<int> &leftRow,vector<int> lowerD, vector<int> upperD){
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(leftRow[i]+lowerD[i+col]+upperD[n-1+col-i]==0){
+                leftRow[i] = 1;
+                lowerD[i+col] = 1;
+                upperD[n-1+col-i] = 1;
+                board[i][col] = 'Q';
+                solve(col+1,n,ans,board,leftRow,lowerD,upperD);
+                leftRow[i] = 0;
+                lowerD[i+col] = 0;
+                upperD[n-1+col-i] = 0;
+                board[i][col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++)    board[i]=s;
+        vector<int> leftRow(n,0);
+        vector<int> lowerD(2*n-1,0), upperD(2*n-1,0);
+        solve(0,n,ans,board,leftRow,lowerD,upperD);
+        return ans;
+    }
+};
+// @lc code=end
+
