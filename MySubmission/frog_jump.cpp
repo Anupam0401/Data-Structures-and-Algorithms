69b003f7f733b9f305959356ea607729fb58229d
Anupam <anupamk@iitbhilai.ac.in>
2022-06-07 23:55:35 +0530

    problem statement

---
+++ MySubmission/frog_jump.cpp
@@ -0,0 +1,9 @@
+// Problem Statement
+// There is a frog on the 1st step of an N stairs long staircase. The frog wants to reach the Nth stair. HEIGHT[i] is the height of the (i+1)th stair.If Frog jumps from ith to jth stair, the energy lost in the jump is given by |HEIGHT[i-1] - HEIGHT[j-1] |.In the Frog is on ith staircase, he can jump either to (i+1)th stair or to (i+2)th stair. Your task is to find the minimum total energy used by the frog to reach from 1st stair to Nth stair.
+
+#include <bits/stdc++.h>
+
+int frogJump(int n, vector<int> &heights)
+{
+
+}
