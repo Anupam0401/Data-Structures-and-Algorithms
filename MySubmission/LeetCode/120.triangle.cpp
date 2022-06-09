9082f9de679334e18e80da9aa68f009b2bacb1e4
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-09 18:30:01 +0530

    done using 1D dp

--- MySubmission/LeetCode/120.triangle.cpp
+++ MySubmission/LeetCode/120.triangle.cpp
@@ -5,14 +5,22 @@
  */
 
 // @lc code=start
+
+// Given a triangle array, return the minimum path sum from top to bottom.
+
+// For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int row = triangle.size();
-        int col = triangle[row-1].size();
-        if(row==1)  return triangle[0][0];
-        vector<vector<int>> dp(row,vector<int>(col,-1));
-        return solve(n-1,)
+        int n = triangle.size();
+        if(n==1)    return triangle[0][0];
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] = triangle[i][j] + min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            }
+        }
+        return triangle[0][0];
     }
 };
 // @lc code=end
