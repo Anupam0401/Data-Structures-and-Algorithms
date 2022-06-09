cc7436e645aa01b3c3c2a8bc66bb872a4945e839
Anupam <anupamk@iitbhilai.ac.in>
2022-06-09 22:54:25 +0530

    added LIS DP solution

--- MySubmission/LeetCode/300.longest-increasing-subsequence.cpp
+++ MySubmission/LeetCode/300.longest-increasing-subsequence.cpp
@@ -4,11 +4,25 @@
  * [300] Longest Increasing Subsequence
  */
 
+// Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
 // @lc code=start
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        
+        int n = nums.size();
+        if(n==0) return 0;
+        vector<int> dp(n,1);
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[i]>nums[j]) dp[i] = max(dp[i],dp[j]+1);
+            }
+        }
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            ans = max(ans,dp[i]);
+        }
+        return ans;
     }
 };
 // @lc code=end
