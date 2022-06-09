318925365af2384d94abc8460023986bcb500351
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-09 18:23:15 +0530

    yet to complete

--- MySubmission/LeetCode/120.triangle.cpp
+++ MySubmission/LeetCode/120.triangle.cpp
@@ -8,7 +8,11 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        
+        int row = triangle.size();
+        int col = triangle[row-1].size();
+        if(row==1)  return triangle[0][0];
+        vector<vector<int>> dp(row,vector<int>(col,-1));
+        return solve(n-1,)
     }
 };
 // @lc code=end
