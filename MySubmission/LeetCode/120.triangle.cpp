fa9ea39c33ea734b3e915de0c5615b819fe9c290
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-09 11:23:16 +0530

    triangle problem init

---
+++ MySubmission/LeetCode/120.triangle.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=120 lang=cpp
+ *
+ * [120] Triangle
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+    }
+};
+// @lc code=end
+
