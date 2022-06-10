d1065b5a6174c5722a30433302c1f449bc4eeb42
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-10 14:43:49 +0530

    rectangle area problem in progress

--- MySubmission/LeetCode/84.largest-rectangle-in-histogram.cpp
+++ MySubmission/LeetCode/84.largest-rectangle-in-histogram.cpp
@@ -8,7 +8,18 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        
+        int n = heights.size();
+        vector<int> dp(n,1);
+        for(int i=0;i<n;i++){
+            dp[i] = heights[i];
+        }
+        int maxArea = 0;
+        for(int i=1;i<n;i++){
+            int minTillNow = dp[i];
+            for(int j=i-1;i >= 0; i--){
+                minTillNow = min(minTillNow,dp[j]);
+            }
+        }
     }
 };
 // @lc code=end
