3180dc1f43d321a1b066126a7658eb2a839566eb
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-13 14:54:16 +0530

    complete using sliding window approach

--- MySubmission/LeetCode/3.longest-substring-without-repeating-characters.cpp
+++ MySubmission/LeetCode/3.longest-substring-without-repeating-characters.cpp
@@ -8,7 +8,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
+        if(s.length()<=1) return s.length();
+        unordered_set<char> uniq;
+        int maxLen = 1,i=0,j=0,n=s.length();
+        while(i<n&&j<n){
+            if(uniq.find(s[i])==uniq.end()){
+                uniq.insert(s[i]);
+                i++;
+                maxLen = max(maxLen,i-j);
+            }
+            else{
+                uniq.erase(s[j]);
+                j++;
+            }
+        }
+        return maxLen;
     }
 };
 // @lc code=end
