92228da691c1d5b360f0f387fd92c898a1c35c32
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-14 18:27:21 +0530

    recursive solution_correct but TLE

--- MySubmission/LeetCode/583.delete-operation-for-two-strings.cpp
+++ MySubmission/LeetCode/583.delete-operation-for-two-strings.cpp
@@ -9,22 +9,26 @@
 // In one step, you can delete exactly one character in either string.
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int minDistance(string word1, string word2) {
-        int n = word1.length(), m = word2.length();
-        if(n==1&&m==1) return !(word1[0]==word2[0]);
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=n;i++) dp[i][0] = i;
-        for(int i=1;i<=m;i++) dp[0][i] = i;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]))+1;
-            }
-        }
-        return dp[n][m];
+    int minDistance(string &word1, string &word2)
+    {
+        return solve(word1, word2, 0, 0);
+    }
+    // recursive solver to find minimum deletions to equalize w1 & w2
+    int solve(string w1, string w2, int i, int j)
+    {
+        if (i == size(w1) && j == size(w2))
+            return 0; // reached end of both strings -> both strings have been equalized and no further steps required
+        if (i == size(w1))
+            return size(w2) - j; // reached end of word1 -> need to delete remaining characters of word2 to equalize
+        if (j == size(w2))
+            return size(w1) - i; // reached end of word2 -> need to delete remaining characters of word1 to equalize
+        if (w1[i] == w2[j])
+            return solve(w1, w2, i + 1, j + 1); // no deletion required since the characters already match
+        // 1 current character deletion + min(after deleting w1[i], after deleting w2[j])
+        return 1 + min(solve(w1, w2, i + 1, j), solve(w1, w2, i, j + 1));
     }
 };
 // @lc code=end
-
