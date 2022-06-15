9cc906bf5ba89619189714f7c693d48d8be7102b
Anupam <anupamk@iitbhilai.ac.in>
2022-06-15 22:46:07 +0530

    probleem init

---
+++ MySubmission/LeetCode/1048.longest-string-chain.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1048 lang=cpp
+ *
+ * [1048] Longest String Chain
+ */
+
+// @lc code=start
+
+// You are given an array of words where each word consists of lowercase English letters.
+
+// wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere in wordA without changing the order of the other characters to make it equal to wordB.
+
+// For example, "abc" is a predecessor of "abac", while "cba" is not a predecessor of "bcad".
+// A word chain is a sequence of words [word1, word2, ..., wordk] with k >= 1, where word1 is a predecessor of word2, word2 is a predecessor of word3, and so on. A single word is trivially a word chain with k == 1.
+
+// Return the length of the longest possible word chain with words chosen from the given list of words.
+
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        
+    }
+};
+// @lc code=end
+
