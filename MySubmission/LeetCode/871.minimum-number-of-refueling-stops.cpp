a2915fa51a3de54180cd6df5f8fdc453e0126f6d
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-16 12:43:51 +0530

    problem init

---
+++ MySubmission/LeetCode/871.minimum-number-of-refueling-stops.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=871 lang=cpp
+ *
+ * [871] Minimum Number of Refueling Stops
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        //recursive solution
+    }
+};
+// @lc code=end
+
