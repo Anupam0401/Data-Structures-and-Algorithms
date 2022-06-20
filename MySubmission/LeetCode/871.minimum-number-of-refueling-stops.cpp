f454189d69386eee78eac79009d91cfc60cfa7f4
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-20 11:35:37 +0530

    still in progress -- implement recursive approach

--- MySubmission/LeetCode/871.minimum-number-of-refueling-stops.cpp
+++ MySubmission/LeetCode/871.minimum-number-of-refueling-stops.cpp
@@ -7,8 +7,14 @@
 // @lc code=start
 class Solution {
 public:
+    int solve(int index, int fuel, int target, int fuel, vector<vector<int>> &stations){
+        if(index==stations.size()){
+            return fuel>=target?0:INT_MAX;
+        }
+    }
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
         //recursive solution
+        solve(0,target,startFuel,stations);                                                       
     }
 };
 // @lc code=end
