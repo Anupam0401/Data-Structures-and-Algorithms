765174eda71d402b546ddf17c07f7947f4ba0ca9
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-22 14:45:29 +0530

    Done using O(1) space and using two temporary nodes

--- MySubmission/LeetCode/206.reverse-linked-list.cpp
+++ MySubmission/LeetCode/206.reverse-linked-list.cpp
@@ -19,27 +19,15 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         if(head==NULL)  return NULL;
-        stack<int> st;
-        while(head->next!= NULL){
-            st.push(head->val);
-            head = head->next;
+          //using O(1) space
+        ListNode *prev = NULL, *curr = head;
+        while(curr!=NULL){
+            ListNode *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
         }
-        // ListNode* new_head = new ListNode(st.top());
-        // st.pop();
-        // ListNode* curr = new_head;
-        // while(!st.empty()){
-        //     curr->next = new ListNode(st.top());
-        //     st.pop();
-        //     curr = curr->next;
-        // }
-        // return new_head;
-        ListNode *temp = head;
-        while(!st.empty()){
-            temp->next = new ListNode(st.top());
-            st.pop();
-            temp = temp->next;
-        }
-        return head;
+        return prev;
     }
 };
 // @lc code=end
