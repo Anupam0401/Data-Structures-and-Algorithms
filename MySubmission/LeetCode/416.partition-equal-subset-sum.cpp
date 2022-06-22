c963648b968699b3e1e01114845b0e79e242980d
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-22 12:35:55 +0530

    done using tabulation

--- MySubmission/LeetCode/416.partition-equal-subset-sum.cpp
+++ MySubmission/LeetCode/416.partition-equal-subset-sum.cpp
@@ -10,27 +10,30 @@ public:
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         int n = nums.size();
+        if(n==1) return false;
         for(int i=0;i<n;i++){
             sum+=nums[i];
         }
         if(sum%2==1)
             return false;
         // doing tabulation - bottom up approach
-        vector<vector<bool>> dp(n, vector<bool>(sum/2+1, false));
+        int target = sum/2;
+        vector<vector<bool>> dp(n, vector<bool>(target+1, false));
         for(int i=0;i<n;i++){
             dp[i][0] = true;
         }
-        dp[0][nums[0]] = true;
+        if(nums[0]<=target)
+            dp[0][nums[0]] = true;
         for(int i=1;i<n;i++){
-            for(int t=1;t<=sum/2;t++){
+            for(int t=1;t<=target;t++){
                 bool not_take = dp[i-1][t];
                 bool take = false;
-                if(t>=nums[i])
+                if(nums[i]<=t)
                     take = dp[i-1][t-nums[i]];
                 dp[i][t] = not_take || take;
             }
         }
-        return dp[n-1][sum/2];
+        return dp[n-1][target];
     }
 };
 // @lc code=end
