8b9f1e3e27a142dc2e1872cf0e935d4345a22a64
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-22 12:45:59 +0530

    done tabulation approach with space optimization

--- MySubmission/LeetCode/416.partition-equal-subset-sum.cpp
+++ MySubmission/LeetCode/416.partition-equal-subset-sum.cpp
@@ -16,24 +16,24 @@ public:
         }
         if(sum%2==1)
             return false;
-        // doing tabulation - bottom up approach
+        // doing tabulation - bottom up approach with space optimization
         int target = sum/2;
-        vector<vector<bool>> dp(n, vector<bool>(target+1, false));
-        for(int i=0;i<n;i++){
-            dp[i][0] = true;
-        }
+        // vector<vector<bool>> dp(n, vector<bool>(target+1, false));
+        vector<bool> prev(target+1, false), curr(target+1, false);
+        prev[0] = curr[0] = true;
         if(nums[0]<=target)
-            dp[0][nums[0]] = true;
+            prev[nums[0]] = true;
         for(int i=1;i<n;i++){
             for(int t=1;t<=target;t++){
-                bool not_take = dp[i-1][t];
+                bool not_take = prev[t];
                 bool take = false;
                 if(nums[i]<=t)
-                    take = dp[i-1][t-nums[i]];
-                dp[i][t] = not_take || take;
+                    take = prev[t-nums[i]];
+                curr[t] = not_take || take;
             }
+            prev = curr;
         }
-        return dp[n-1][target];
+        return prev[target];
     }
 };
 // @lc code=end
