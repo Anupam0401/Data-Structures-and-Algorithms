30f4aeeff6edc2b50622de2d9ed6769757d4b200
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-22 12:26:11 +0530

    tried tabulation --getting run time error

--- MySubmission/LeetCode/416.partition-equal-subset-sum.cpp
+++ MySubmission/LeetCode/416.partition-equal-subset-sum.cpp
@@ -7,18 +7,6 @@
 // @lc code=start
 class Solution {
 public:
-    bool one_partition(int ind, int target, vector<int>& nums, vector<vector<int>>& dp){
-        if(target == 0)
-            return true;
-        if(ind == 0)
-            return nums[ind] == target;
-        if(dp[ind][target] != -1) return dp[ind][target];
-        bool not_take = one_partition(ind-1, target, nums, dp);
-        bool take = false;
-        if(target>=nums[ind])
-            take = one_partition(ind-1, target-nums[ind], nums,dp);
-        return dp[ind][target] = not_take || take;
-    }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         int n = nums.size();
@@ -27,8 +15,22 @@ public:
         }
         if(sum%2==1)
             return false;
-        vector<vector<int>> dp(n+1, vector<int>(sum/2+1, -1));
-        return one_partition(nums.size()-1, sum/2, nums,dp);
+        // doing tabulation - bottom up approach
+        vector<vector<bool>> dp(n, vector<bool>(sum/2+1, false));
+        for(int i=0;i<n;i++){
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        for(int i=1;i<n;i++){
+            for(int t=1;t<=sum/2;t++){
+                bool not_take = dp[i-1][t];
+                bool take = false;
+                if(t>=nums[i])
+                    take = dp[i-1][t-nums[i]];
+                dp[i][t] = not_take || take;
+            }
+        }
+        return dp[n-1][sum/2];
     }
 };
 // @lc code=end
