99d8ce78e3fdb0f31d54c1f24083d9a8c39e4698
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-22 11:10:51 +0530

    done usign dp -- bottom up approach

--- MySubmission/LeetCode/416.partition-equal-subset-sum.cpp
+++ MySubmission/LeetCode/416.partition-equal-subset-sum.cpp
@@ -7,25 +7,28 @@
 // @lc code=start
 class Solution {
 public:
-    bool one_partition(int ind, int target, vector<int>& nums){
+    bool one_partition(int ind, int target, vector<int>& nums, vector<vector<int>>& dp){
         if(target == 0)
             return true;
         if(ind == 0)
             return nums[ind] == target;
-        bool not_take = one_partition(ind-1, target, nums);
+        if(dp[ind][target] != -1) return dp[ind][target];
+        bool not_take = one_partition(ind-1, target, nums, dp);
         bool take = false;
         if(target>=nums[ind])
-            take = one_partition(ind-1, target-nums[ind], nums);
-        return not_take || take;
+            take = one_partition(ind-1, target-nums[ind], nums,dp);
+        return dp[ind][target] = not_take || take;
     }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        for(int i=0;i<nums.size();i++){
+        int n = nums.size();
+        for(int i=0;i<n;i++){
             sum+=nums[i];
         }
         if(sum%2==1)
             return false;
-        return one_partition(nums.size()-1, sum/2, nums);
+        vector<vector<int>> dp(n+1, vector<int>(sum/2+1, -1));
+        return one_partition(nums.size()-1, sum/2, nums,dp);
     }
 };
 // @lc code=end
