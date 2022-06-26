f85c33bff9c3ab59590b608d36ab5561e6f55ccb
Anupam <anupamk@iitbhilai.ac.in>
2022-06-26 16:03:18 +0530

    memoized the recursive solution but now giving MLE

--- MySubmission/LeetCode/1423.maximum-points-you-can-obtain-from-cards.cpp
+++ MySubmission/LeetCode/1423.maximum-points-you-can-obtain-from-cards.cpp
@@ -7,15 +7,17 @@
 // @lc code=start
 class Solution {
 public:
-    int solve(vector<int>& cardPoints,int left, int right, int sum, int k){
+    int solve(vector<int>& cardPoints,int left, int right, int sum, int k,vector<vector<int>>& dp){
         if(k<=0)    return sum;
-        int ls = solve(cardPoints, left+1,right,sum+cardPoints[left],k-1);
-        int rs = solve(cardPoints, left, right-1,sum+cardPoints[right],k-1);
-        return max(ls, rs);
+        if(dp[left][right]!=-1) return dp[left][right];
+        int ls = solve(cardPoints, left+1,right,sum+cardPoints[left],k-1,dp);
+        int rs = solve(cardPoints, left, right-1,sum+cardPoints[right],k-1,dp);
+        return dp[left][right] = max(ls, rs);
     }
     int maxScore(vector<int>& cardPoints, int k) {
         int n = cardPoints.size();
-        return solve(cardPoints,0,n-1,0,k);
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return solve(cardPoints,0,n-1,0,k,dp);
     }
 };
 // @lc code=end
