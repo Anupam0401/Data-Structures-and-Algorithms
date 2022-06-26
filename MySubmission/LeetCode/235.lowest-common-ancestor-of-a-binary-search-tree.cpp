dd4bad730712f680523806911569a206fe52ea9a
Anupam <anupamk@iitbhilai.ac.in>
2022-06-26 21:44:26 +0530

    done using iteration -- finding the common subtree

--- MySubmission/LeetCode/235.lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ MySubmission/LeetCode/235.lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -18,18 +18,9 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == NULL) {
-            return NULL;
-        }
-        if (root == p || root == q) {
-            return root;
-        }
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        if (left && right) {
-            return root;
-        }
-        return left ? left : right;
+        while((p->val - root->val) * (q->val-root->val)>0)
+            root = p->val < root->val ? root->left : root->right;
+        return root;
     }
 };
 // @lc code=end
