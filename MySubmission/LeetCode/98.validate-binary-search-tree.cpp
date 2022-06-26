47d18a171196a780af908e230694303b63fb176e
Anupam <anupamk@iitbhilai.ac.in>
2022-06-26 21:15:08 +0530

    done using checking the validation of inorder traversal

---
+++ MySubmission/LeetCode/98.validate-binary-search-tree.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorderTraversal(TreeNode* root, vector<int>& inorder){
+        if(root == nullptr) return;
+        inorderTraversal(root->left, inorder);
+        inorder.push_back(root->val);
+        inorderTraversal(root->right, inorder);
+    }
+    bool isValidBST(TreeNode* root) {
+        vector<int> inorder;
+        inorderTraversal(root, inorder);
+        for(int i=0;i<inorder.size()-1;i++){
+            if(inorder[i]>= inorder[i+1]) return false;
+        }
+        return true;
+    }
+};
+// @lc code=end
+
