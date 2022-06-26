bd988d509b4de92397f92bf7ea657f822ff5b058
Anupam <anupamk@iitbhilai.ac.in>
2022-06-26 16:36:08 +0530

    done using iteration

--- MySubmission/LeetCode/704.binary-search.cpp
+++ MySubmission/LeetCode/704.binary-search.cpp
@@ -7,15 +7,16 @@
 // @lc code=start
 class Solution {
 public:
-    int binary(int left,int right, vector<int>& nums, int target){
-        if(left>right) return -1;
-        int mid = (left+right)/2;
-        if(nums[mid]==target) return mid;
-        if(nums[mid]<target) return binary(mid+1,right,nums,target);
-        else return binary(left,mid-1,nums,target);
-    }
     int search(vector<int>& nums, int target) {
-        return binary(0, nums.size()-1, nums, target);
+        int n = nums.size();
+        int left = 0, right = n-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] < target) left = mid+1;
+            else right = mid-1;
+        }
+        return -1;
     }
 };
 // @lc code=end
