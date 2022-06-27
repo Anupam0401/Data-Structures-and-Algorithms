431d058393305f8eb39f0099cac8b863a5190811
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-27 11:58:39 +0530

    done using tabulation and memory optimized

--- MySubmission/LeetCode/509.fibonacci-number.cpp
+++ MySubmission/LeetCode/509.fibonacci-number.cpp
@@ -7,15 +7,14 @@
 // @lc code=start
 class Solution {
 public:
-    int find_fib(int n, vector<int>& dp){
-        if(n==0||n==1)  return n;
-        if(dp[n]!=-1)    return dp[n];
-        return dp[n] = find_fib(n-1,dp)+find_fib(n-2,dp);
-    }
     int fib(int n) {
         if(n==0||n==1)  return n;
-        vector<int> dp(n+1,-1);
-        return find_fib(n,dp);
+        int a=0,b=1;
+        for(int i=2;i<=n;i++){
+            b=a+b;
+            a=b-a;
+        }
+        return b;
     }
 };
 // @lc code=end
