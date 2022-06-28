d31eb6ac54e17a4c573f1df3f0f1ef71b029488b
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-28 14:38:07 +0530

    corrected the syntax error

--- MySubmission/LeetCode/746.min-cost-climbing-stairs.cpp
+++ MySubmission/LeetCode/746.min-cost-climbing-stairs.cpp
@@ -5,21 +5,24 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        int get_minCost(int index,vector<int>& cost, vector<int>& dp){
-        if(index<0) return 0;
-        if(index==0||index==1)  return cost[index];
-        if(dp[index]!=-1)   return dp[index];
-        return dp[index] = cost[index]+min(get_minCost(index-1,cost,dp),get_minCost(index-2,cost,dp));
+    int get_minCost(int index, vector<int> &cost, vector<int> &dp)
+    {
+        if (index < 0)
+            return 0;
+        if (index == 0 || index == 1)
+            return cost[index];
+        if (dp[index] != -1)
+            return dp[index];
+        return dp[index] = cost[index] + min(get_minCost(index - 1, cost, dp), get_minCost(index - 2, cost, dp));
     }
-    int minCostClimbingStairs(vector<int>& cost) {
+    int minCostClimbingStairs(vector<int> &cost)
+    {
         int n = cost.size();
-        vector<int> dp(n,-1);
-        return min(get_minCost(n-1,cost,dp),get_minCost(n-2,cost,dp));
-    }
+        vector<int> dp(n, -1);
+        return min(get_minCost(n - 1, cost, dp), get_minCost(n - 2, cost, dp));
     }
 };
 // @lc code=end
-
