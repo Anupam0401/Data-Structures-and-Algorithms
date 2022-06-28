6687ac40a1855945fdb346f9fb503e2ba3c0c6eb
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-28 14:46:06 +0530

    done using tabulation -- space optimized to O(1)

--- MySubmission/LeetCode/746.min-cost-climbing-stairs.cpp
+++ MySubmission/LeetCode/746.min-cost-climbing-stairs.cpp
@@ -11,15 +11,14 @@ public:
     int minCostClimbingStairs(vector<int> &cost)
     {
         int n = cost.size();
-        vector<int> dp(n, -1);
-        for (int i = 0; i < n; i++)
+        int a=cost[0],b=cost[1];
+        for (int i = 2; i < n; i++)
         {
-            if (i == 0 || i == 1)
-                dp[i] = cost[i];
-            else
-                dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+            int c = cost[i] + min(a,b);
+            a=b;
+            b=c;
         }
-        return min(dp[n - 1], dp[n - 2]);
+        return min(a,b);
     }
 };
 // @lc code=end
