227387afdc94b878e2022f165ae575d4587eaa6f
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-29 11:17:51 +0530

    lengthy code but intuitive -- done using stack

--- MySubmission/LeetCode/101.symmetric-tree.cpp
+++ MySubmission/LeetCode/101.symmetric-tree.cpp
@@ -19,19 +19,40 @@
 class Solution
 {
 public:
-    bool check_symmetry(TreeNode *leftst, TreeNode *rightst)
-    {
-        if (leftst == NULL || rightst == NULL)
-            return leftst == rightst;
-        if (leftst->val != rightst->val)
-            return false;
-        return check_symmetry(leftst->left, rightst->right) && check_symmetry(leftst->right, rightst->left);
-    }
     bool isSymmetric(TreeNode *root)
     {
         if (root == NULL)
             return true;
-        return check_symmetry(root->left, root->right);
+        // using stack
+        if (root->left == NULL || root->right == NULL)
+            return root->left == root->right;
+        stack<TreeNode *> st;
+        if (root->left->val != root->right->val)
+            return false;
+        st.push(root->left);
+        st.push(root->right);
+        while(!st.empty()){
+            if(st.size()%2!=0)  return false;
+            TreeNode *rsubtree = st.top();
+            st.pop();
+            TreeNode *lsubtree = st.top();
+            st.pop();
+            if(lsubtree==NULL or rsubtree==NULL)    return lsubtree == rsubtree;
+            else if(lsubtree->val!=rsubtree->val)   return false;
+            if(lsubtree->left!=NULL){
+                if(rsubtree->right==NULL)   return false;
+                st.push(lsubtree->left);
+                st.push(rsubtree->right);
+            }
+            else if(rsubtree->right!=NULL)  return false;
+            if(lsubtree->right!=NULL){
+                if(rsubtree->left==NULL)    return false;
+                st.push(lsubtree->right);
+                st.push(rsubtree->left);
+            }
+            else if(rsubtree->left!=NULL)   return false;
+        }
+        return true;
     }
 };
 // @lc code=end
