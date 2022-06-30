0dae2821174080c86a19f1b62042ac6750e8546d
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-30 15:55:48 +0530

    dfs and recusive approach

---
+++ MySubmission/LeetCode/226.invert-binary-tree.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=226 lang=cpp
+ *
+ * [226] Invert Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)  return root;
+        TreeNode *leftst = root->left, *rightst = root->right; 
+        root->left = invertTree(rightst);
+        root->right = invertTree(leftst);
+        return root;
+    }
+};
+// @lc code=end
+
