d225b32206e709a82163732e7c4f62d7bbf30ade
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-06-30 18:31:45 +0530

    done using recursion

---
+++ MySubmission/LeetCode/700.search-in-a-binary-search-tree.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=700 lang=cpp
+ *
+ * [700] Search in a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL || val==root->val)  return root;
+        else if(root->val>val)  return searchBST(root->left,val);
+        else return searchBST(root->right,val);
+    }
+};
+// @lc code=end
+
