5a0adee4c84475fdf0c6dbd61e6160e7a68b7653
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-07-01 18:35:06 +0530

    must do this question some other time

--- MySubmission/LeetCode/36.valid-sudoku.cpp
+++ MySubmission/LeetCode/36.valid-sudoku.cpp
@@ -10,55 +10,22 @@ class Solution
 public:
     bool isValidSudoku(vector<vector<char>> &board)
     {
-        // check row wise
+        vector<short> col(9, 0);
+        vector<short> block(9, 0);
+        vector<short> row(9, 0);
         for (int i = 0; i < 9; i++)
-        {
-            unordered_set<int> s;
             for (int j = 0; j < 9; j++)
             {
                 if (board[i][j] != '.')
                 {
-                    int num = board[i][j] - '0';
-                    if (s.find(num) != s.end())
+                    int idx = 1 << (board[i][j] - '0');
+                    if (row[i] & idx || col[j] & idx || block[i / 3 * 3 + j / 3] & idx)
                         return false;
-                    s.insert(num);
+                    row[i] |= idx;
+                    col[j] |= idx;
+                    block[i / 3 * 3 + j / 3] |= idx;
                 }
             }
-        }
-        // check column wise
-        for (int j = 0; j < 9; j++)
-        {
-            unordered_set<int> s;
-            for (int i = 0; i < 9; i++)
-            {
-                if (board[i][j] != '.')
-                {
-                    int num = board[i][j] - '0';
-                    if (s.find(num) != s.end())
-                        return false;
-                    s.insert(num);
-                }
-            }
-        }
-        // check 3x3
-        for (int i = 0; i < 9; i += 3)
-        {
-            for (int j = 0; j < 9; j += 3)
-            {
-                unordered_set<int> s;
-                for (int p = i; p < i + 3; p++)
-                {
-                    for (int q = j; q < j + 3; q++)
-                    {
-                        cout<<p<<" "<<q<<endl;
-                        int num = board[p][q] - '0';
-                        if (s.find(num) != s.end())
-                            return false;
-                        s.insert(num);
-                    }
-                }
-            }
-        }
         return true;
     }
 };
