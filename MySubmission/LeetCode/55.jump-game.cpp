182c5dac22669aaef650ddf595a11aa3fb8a621a
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-07-05 15:37:44 +0530

    done using greedy approach by definition of reach of each index

--- MySubmission/LeetCode/55.jump-game.cpp
+++ MySubmission/LeetCode/55.jump-game.cpp
@@ -5,22 +5,37 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    bool helper(int ind, vector<int>& nums, vector<int>& dp){
-        if(ind >= nums.size()-1) return true;
-        
-        if(dp[ind] != -1) return dp[ind];
+    bool helper(int ind, vector<int> &nums, vector<int> &dp)
+    {
+        if (ind >= nums.size() - 1)
+            return true;
+
+        if (dp[ind] != -1)
+            return dp[ind];
         bool val = false;
-        for(int i=1; i<=nums[ind]; i++){
+        for (int i = 1; i <= nums[ind]; i++)
+        {
             val = val || helper(ind + i, nums, dp);
         }
         return dp[ind] = val;
     }
-    bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return helper(0, nums, dp);
+    bool canJump(vector<int> &nums)
+    {
+        // greedy appproach
+        int n = nums.size();
+        if (n == 1)
+            return true;
+        int reach = nums[0];
+        for (int i = 1; i < n - 1; i++)
+        {
+            if (reach < i)
+                return false;
+            reach = max(nums[i] + i, reach);
+        }
+        return reach >= n - 1;
     }
 };
 // @lc code=end
-
