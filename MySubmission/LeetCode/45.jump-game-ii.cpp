7d69df7c3fbab618ae6ece2f8a7bdd35802e21ed
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-07-05 16:29:38 +0530

    done using recursion and memoization

---
+++ MySubmission/LeetCode/45.jump-game-ii.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=45 lang=cpp
+ *
+ * [45] Jump Game II
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int solve(int ind, vector<int> &nums, vector<int> &dp)
+    {
+        if (ind >= nums.size() - 1)
+            return 0;
+        if (dp[ind] != 10001)
+            return dp[ind];
+        for (int i = 1; i <= nums[ind]; i++)
+        {
+            dp[ind] = min(dp[ind], 1 + solve(ind + i, nums, dp));
+        }
+        return dp[ind];
+    }
+    int jump(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 1)
+            return 0;
+        vector<int> dp(n, 10001);
+        return solve(0, nums, dp);
+    }
+};
+// @lc code=end
