599850e2f085e2cf4529d441ff32d25f786b5ee5
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-07-05 14:25:58 +0530

    done using tabulation, also coded out the space optimization of the same

--- MySubmission/LeetCode/740.delete-and-earn.cpp
+++ MySubmission/LeetCode/740.delete-and-earn.cpp
@@ -14,18 +14,47 @@ private:
 public:
     int solve(int n)
     {
-        if(n==0)    return 0;
-        if(n==1){
-            if(hash.find(1)!=hash.end())    return hash[1];
+        if (n == 0)
+            return 0;
+        if (n == 1)
+        {
+            if (hash.find(1) != hash.end())
+                return hash[1];
             return 0;
         }
-        if(dp.find(n)!=dp.end())    return dp[n];
+        if (dp.find(n) != dp.end())
+            return dp[n];
         int gain = 0;
-        if(hash.find(n)!=hash.end())    gain=hash[n];
-        return dp[n] = max(solve(n-1),solve(n-2)+gain);
+        if (hash.find(n) != hash.end())
+            gain = hash[n];
+        return dp[n] = max(solve(n - 1), solve(n - 2) + gain);
     }
-    int tabulation(int n){
-        
+    int tabulation(int n)
+    {
+        dp[0] = 0;
+        dp[1] = hash.find(1) != hash.end() ? hash[1] : 0;
+        for (int i = 2; i <= n; i++)
+        {
+            int gain = 0;
+            if (hash.find(i) != hash.end())
+                gain = hash[i];
+            dp[i] = max(dp[i - 1], gain + dp[i - 2]);
+        }
+        return dp[n];
+    }
+    int spaceOptimizedTabulation(int n){
+        int a = 0;
+        int b = hash.find(1) != hash.end() ? hash[1] : 0;
+        for (int i = 2; i <= n; i++)
+        {
+            int gain = 0;
+            if (hash.find(i) != hash.end())
+                gain = hash[i];
+            int c = max(b, gain + a);
+            a=b;
+            b=c;
+        }
+        return b;
     }
     int deleteAndEarn(vector<int> &nums)
     {
@@ -36,7 +65,9 @@ public:
             hash[x] += x;
             maxNum = max(maxNum, x);
         }
-        return solve(maxNum);
+        // return solve(maxNum);
+        // return tabulation(maxNum);
+        return spaceOptimizedTabulation(maxNum);
     }
 };
 // @lc code=end
