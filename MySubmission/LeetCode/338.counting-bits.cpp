f0bd259b1226987c280ece8e3cc6751ac3d5cac6
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-07-07 12:11:49 +0530

    done using bit manipluation in O(n) TC

--- MySubmission/LeetCode/338.counting-bits.cpp
+++ MySubmission/LeetCode/338.counting-bits.cpp
@@ -5,21 +5,18 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<int> countBits(int n) {
-        vector<int> ans(n+1,0);
-        for(int i=1;i<=n;i++){
-            int sum = 0;
-            int num = i;
-            while(num){
-                sum+=num%2;
-                num/=2;
-            }
-            ans[i]=sum;
+    vector<int> countBits(int n)
+    {
+        vector<int> ans(n + 1, 0);
+        // using bit manipluation
+        for (int i = 1; i <= n; i++)
+        {
+            ans[i] = ans[i / 2] + i % 2;
         }
         return ans;
     }
 };
 // @lc code=end
-
