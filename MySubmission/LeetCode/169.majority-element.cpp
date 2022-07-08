4774e4894a0d5e917e68ed6e5d6ae0bba391008c
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-07-08 17:06:53 +0530

    done using moore's voting algorithm -- O(n) time and O(1) space

--- MySubmission/LeetCode/169.majority-element.cpp
+++ MySubmission/LeetCode/169.majority-element.cpp
@@ -5,19 +5,24 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int majorityElement(vector<int>& nums) {
-        set<int> S;
-        for(int i=0; i<nums.size(); i++){
-            if(S.find(nums[i]) == S.end()){
-                S.insert(nums[i]);
+    int majorityElement(vector<int> &nums)
+    {
+        int n = nums.size();
+        if(n==1)    return nums[0];
+        int majority = nums[0];
+        int count = 1;
+        for(int i=1;i<n;i++){
+            if(count == 0){
+                majority = nums[i];
+                count++;
             }
-            else{
-                S.erase(nums[i]);
-            }
+            else if(nums[i]==majority)  count++;
+            else count--;
         }
+        return majority;
     }
 };
 // @lc code=end
-
