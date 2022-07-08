c880d6d7bdaef22e40082183876bf3914f5b9208
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-07-08 15:58:26 +0530

    most optimized approach using binary search on the whole array

--- MySubmission/LeetCode/74.search-a-2-d-matrix.cpp
+++ MySubmission/LeetCode/74.search-a-2-d-matrix.cpp
@@ -10,15 +10,31 @@ class Solution
 public:
     bool searchMatrix(vector<vector<int>> &matrix, int target)
     {
-        int i = 0, j = matrix[0].size() - 1;
-        while (i < matrix.size() && j >= 0)
+        // int i = 0, j = matrix[0].size() - 1;
+        // while (i < matrix.size() && j >= 0)
+        // {
+        //     if (matrix[i][j] == target)
+        //         return true;
+        //     else if (matrix[i][j] > target)
+        //         j--;
+        //     else if (matrix[i][j] < target)
+        //         i++;
+        // }
+        // return false;
+        // using binary search on the whole matrix at once
+        // Time complexity: O(log(rw*cl))
+        int r = matrix.size(), c = matrix[0].size();
+        int left = 0, right = (r * c) - 1;
+        while (left <= right)
         {
+            int mid = (left + (right - left) / 2);
+            int i = mid / c, j = mid % c;
             if (matrix[i][j] == target)
                 return true;
-            else if (matrix[i][j] > target)
-                j--;
             else if (matrix[i][j] < target)
-                i++;
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
         return false;
     }
