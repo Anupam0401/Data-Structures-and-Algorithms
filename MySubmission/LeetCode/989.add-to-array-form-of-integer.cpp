94ddf6ab28abd9029c5b0b56c4bfc28aaea083e4
Anupam <anupamk@iitbhilai.ac.in>
2022-07-17 22:43:20 +0530

    done in O(1) space and in O(n) TC

--- MySubmission/LeetCode/989.add-to-array-form-of-integer.cpp
+++ MySubmission/LeetCode/989.add-to-array-form-of-integer.cpp
@@ -7,28 +7,43 @@
 // @lc code=start
 class Solution {
 public:
+//using extra memory
+    // vector<int> addToArrayForm(vector<int>& num, int k) {
+    //     int n = num.size();
+    //     int i = n-1;
+    //     int carry = 0, sum = 0;
+    //     vector<int> res;
+    //     while(i>=0 || k>0){
+    //         sum = carry;
+    //         if(i>=0){
+    //             sum += num[i];
+    //             i--;
+    //         }
+    //         if(k>0){
+    //             sum += k%10;
+    //             k = k/10;
+    //         }
+    //         carry = sum/10;
+    //         sum = sum%10;
+    //         res.push_back(sum);
+    //     }
+    //     if(carry)   res.push_back(carry);
+    //     reverse(res.begin(),res.end());
+    //     return res;
+    // }
+
+    // O(1) space
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        int n = num.size();
-        int i = n-1;
-        int carry = 0, sum = 0;
-        vector<int> res;
-        while(i>=0 || k>0){
-            sum = carry;
-            if(i>=0){
-                sum += num[i];
-                i--;
-            }
-            if(k>0){
-                sum += k%10;
-                k = k/10;
-            }
-            carry = sum/10;
-            sum = sum%10;
-            res.push_back(sum);
+        for(int i = num.size()-1;i>=0 && k>0; --i){
+            num[i] += k;
+            k = num[i]/10;
+            num[i] = num[i]%10; 
         }
-        if(carry)   res.push_back(carry);
-        reverse(res.begin(),res.end());
-        return res;
+        while(k){
+            num.insert(num.begin(),k%10);
+            k/=10;
+        }
+        return num;
     }
 };
 // @lc code=end
