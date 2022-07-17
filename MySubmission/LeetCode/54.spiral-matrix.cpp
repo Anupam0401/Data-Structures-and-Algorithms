4989b783832b54bd92f0fd47311892e657da6f43
Anupam <anupamk@iitbhilai.ac.in>
2022-07-18 00:15:00 +0530

    done with the help of direction and made case for each of direction

--- MySubmission/LeetCode/54.spiral-matrix.cpp
+++ MySubmission/LeetCode/54.spiral-matrix.cpp
@@ -8,13 +8,42 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        int col = matrix[0].size();
-        int row = matrix.size();
-        while(row!=0||col!=0) {
-            ans.push_back(matrix[row][col]);
-            if()
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int left=0,right=m-1,bottom=n-1,top=0;
+        int direction=1;
+        vector<int> v;
+        while(left<=right && top<=bottom)
+        {
+            if(direction==1)
+            {
+                for(int i=left;i<=right;i++) v.push_back(matrix[top][i]);
+                direction=2;
+                top++;
+            }
+            
+            else if(direction==2)
+            {
+                for(int i=top;i<=bottom;i++) v.push_back(matrix[i][right]);
+                direction=3;
+                right--;
+            }
+            
+            else if(direction==3)
+            {
+                for(int i=right;i>=left;i--) v.push_back(matrix[bottom][i]);
+                direction=4;
+                bottom--;
+            }
+            
+            else if(direction==4)
+            {
+                for(int i=bottom;i>=top;i--) v.push_back(matrix[i][left]);
+                direction=1;
+                left++;
+            }
         }
+        return v;
     }
 };
 // @lc code=end
