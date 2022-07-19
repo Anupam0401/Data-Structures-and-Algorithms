df72c78f918c4c92258dd39a9ea2e9eb46c57f52
Anupam <anupamk@iitbhilai.ac.in>
2022-07-20 01:28:55 +0530

    done the naive approach, TLE

---
+++ MySubmission/LeetCode/61.rotate-list.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=61 lang=cpp
+ *
+ * [61] Rotate List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // naive, brute force approach
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(k==0 || !head || !head->next)  return head;
+        while (k--)
+        {
+            ListNode *last = head;
+            int valu = 0;
+            while(last->next!=NULL){
+                if(last->next->next==NULL){
+                    valu = last->next->val;
+                    last->next = NULL;
+                    break;
+                }
+                last=last->next;
+            }
+            ListNode *newNode = new ListNode(valu);
+            newNode->next = head;
+            head = newNode;
+            if(k==0)    return newNode;
+        }
+        return head;
+    }
+
+    
+};
+// @lc code=end
+
