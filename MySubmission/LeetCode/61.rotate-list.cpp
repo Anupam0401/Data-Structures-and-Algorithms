10f681c00e340a361a63736e64a733924eb38654
Anupam <anupamk@iitbhilai.ac.in>
2022-07-20 01:35:18 +0530

    optimised it to O(n)  -- done using finding correct pos of head and placing the null correctly

--- MySubmission/LeetCode/61.rotate-list.cpp
+++ MySubmission/LeetCode/61.rotate-list.cpp
@@ -18,29 +18,43 @@
 class Solution {
 public:
     // naive, brute force approach
+    // ListNode* rotateRight(ListNode* head, int k) {
+    //     if(k==0 || !head || !head->next)  return head;
+    //     while (k--)
+    //     {
+    //         ListNode *last = head;
+    //         int valu = 0;
+    //         while(last->next!=NULL){
+    //             if(last->next->next==NULL){
+    //                 valu = last->next->val;
+    //                 last->next = NULL;
+    //                 break;
+    //             }
+    //             last=last->next;
+    //         }
+    //         ListNode *newNode = new ListNode(valu);
+    //         newNode->next = head;
+    //         head = newNode;
+    //         if(k==0)    return newNode;
+    //     }
+    //     return head;
+    // }
+
+    // optimised O(n) approach
     ListNode* rotateRight(ListNode* head, int k) {
         if(k==0 || !head || !head->next)  return head;
-        while (k--)
-        {
-            ListNode *last = head;
-            int valu = 0;
-            while(last->next!=NULL){
-                if(last->next->next==NULL){
-                    valu = last->next->val;
-                    last->next = NULL;
-                    break;
-                }
-                last=last->next;
-            }
-            ListNode *newNode = new ListNode(valu);
-            newNode->next = head;
-            head = newNode;
-            if(k==0)    return newNode;
-        }
+        int len = 1;
+        ListNode *temp = head;
+        while (temp->next!=NULL and ++len)
+            temp=temp->next;
+        temp->next = head;
+        k = k%len;
+        k = len-k;
+        while(k--)  temp = temp->next;
+        head = temp->next;
+        temp->next = NULL;
         return head;
     }
-
-    
 };
 // @lc code=end
 
