bbfec2efb953afa15918f7d9d3efdd16095d1dee
Anupam <anupamk@iitbhilai.ac.in>
2022-07-20 00:43:07 +0530

    done in O(1) space in 3 steps of finding middle element, reversing the right part and then checking

--- MySubmission/LeetCode/234.palindrome-linked-list.cpp
+++ MySubmission/LeetCode/234.palindrome-linked-list.cpp
@@ -17,18 +17,49 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        // brute force, highly uneconomical approach
-        vector<int> nums;
-        ListNode* temp = head;
-        while (temp!=NULL)
-        {   
-            nums.push_back(temp->val);
-            temp = temp->next;
+    // bool isPalindrome(ListNode* head) {
+    //     // brute force, highly uneconomical approach
+    //     vector<int> nums;
+    //     ListNode* temp = head;
+    //     while (temp!=NULL)
+    //     {   
+    //         nums.push_back(temp->val);
+    //         temp = temp->next;
+    //     }
+    //     int n = nums.size();
+    //     for(int i=0;i<=(n-1)/2;i++){
+    //         if(nums[i]!=nums[n-1-i])    return false;
+    //     }
+    //     return true;
+    // }
+    ListNode* reverse(ListNode* head){
+        ListNode *prev = NULL, *curr =  head;
+        while(curr!=NULL){
+            ListNode *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
         }
-        int n = nums.size();
-        for(int i=0;i<=(n-1)/2;i++){
-            if(nums[i]!=nums[n-1-i])    return false;
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL || head->next==NULL)  return true;
+        // finding the middle of the linked list
+        ListNode *slow = head, *fast = head;
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        slow->next = reverse(slow->next);
+        slow = slow->next;
+        
+        while(slow!=NULL){
+            cout<<slow->val<<" "<<head->val;
+            if(slow->val!=head->val){
+                return false;
+            }
+            slow = slow->next;
+            head = head->next;
         }
         return true;
     }
