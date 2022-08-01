58d4dba9ee9b9fedc6fbdbbd54ec9d0442a9a599
Anupam <anupamk@iitbhilai.ac.in>
2022-08-01 22:44:59 +0530

    memoization approach

--- MySubmission/LeetCode/62.unique-paths.cpp
+++ MySubmission/LeetCode/62.unique-paths.cpp
@@ -8,20 +8,14 @@
 class Solution
 {
 public:
-    int uniquePaths(int m, int n)
-    {
-        if(m==1&&n==1 || m==2&&n==1 || m==1&&n==2)  return 1;
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        dp[0][1] = 1;
-        // dp[1][0] = 1;
-        for (int i = 1; i <= m; i++)
-        {
-            for (int j = 1; j <= n; j++)
-            {
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
-            }
-        }
-        return dp[m][n];
+    int solve(int m,int n,vector<vector<int>>& dp){
+        if(m==0 || n==0)    return 1;
+        if(dp[m][n]!=-1)    return dp[m][n];
+        return dp[m][n] = solve(m-1,n,dp)+solve(m,n-1,dp);
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp);
     }
 };
 // @lc code=end
