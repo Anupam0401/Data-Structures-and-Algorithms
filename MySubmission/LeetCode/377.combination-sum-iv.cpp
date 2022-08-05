7bbece0da03159a655a3db9493ea4ad8dbf23455
Anupam <anupamk@iitbhilai.ac.in>
2022-08-06 00:55:51 +0530

    done using tabulation in O(n*t) TC and O(T) SC

--- MySubmission/LeetCode/377.combination-sum-iv.cpp
+++ MySubmission/LeetCode/377.combination-sum-iv.cpp
@@ -14,16 +14,25 @@ public:
             return 1;
         if(dp[target]!=-1)  return dp[target];
         int ans = 0;
-        for(auto &i:nums){
-            if(i<=target)   ans+=count(nums,target-i,dp);
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<=target)   ans+=count(nums,target-nums[i],dp);
         }
         return dp[target] = ans;
     }
     int combinationSum4(vector<int> &nums, int target)
     {
         int n = nums.size();
-        vector<int> dp(target+1,-1);
-        return count(nums, target, dp);
+        vector<int> dp(target+1,0);
+        // return count(nums, target, dp);
+        // tabulation approach
+        dp[0] = 1; // for target=0, we have 1 subset as the base case
+        for(int t = 1;t<=target;t++){
+            for(int i=0;i<n;i++){
+                if(nums[i]<=t)
+                    dp[t] +=dp[t-nums[i]];
+            }
+        }
+        return dp[target];
     }
 };
 // @lc code=end
