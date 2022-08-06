57604820f4a02b531ee613f678cfd87bad39b0cd
Anupam <anupamk@iitbhilai.ac.in>
2022-08-06 11:15:29 +0530

    done using priority queue in O(n) TC and SC

--- MySubmission/LeetCode/347.top-k-frequent-elements.cpp
+++ MySubmission/LeetCode/347.top-k-frequent-elements.cpp
@@ -7,25 +7,41 @@
 // @lc code=start
 class Solution {
 public:
-    static bool comp(vector<int> &a, vector<int> &b){
-        return a[1]>b[1];
-    }
+    // static bool comp(vector<int> &a, vector<int> &b){
+    //     return a[1]>b[1];
+    // }
+    // vector<int> topKFrequent(vector<int>& nums, int k) {
+    //     unordered_map<int,int> mp;
+    //     int n = nums.size();
+    //     for(auto num:nums)  mp[num]++;
+    //     vector<vector<int>> freq;
+    //     for(auto &it : mp){
+    //         freq.push_back({it.first,it.second});
+    //     }
+    //     sort(freq.begin(),freq.end(),comp);
+    //     vector<int> res;
+    //     for(auto it:freq){
+    //         if(k--) res.push_back(it[0]);
+    //         else break;
+    //     }
+    //     return res;
+    // }
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> mp;
         int n = nums.size();
         for(auto num:nums)  mp[num]++;
-        vector<vector<int>> freq;
-        for(auto &it : mp){
-            freq.push_back({it.first,it.second});
+        priority_queue<pair<int,int>> pq;
+        for(auto &it: mp){
+            pq.push(make_pair(it.second,it.first));
         }
-        sort(freq.begin(),freq.end(),comp);
         vector<int> res;
-        for(auto it:freq){
-            if(k--) res.push_back(it[0]);
-            else break;
+        while(k--){
+            res.push_back(pq.top().second);
+            pq.pop();
         }
         return res;
     }
+
 };
 // @lc code=end
 
