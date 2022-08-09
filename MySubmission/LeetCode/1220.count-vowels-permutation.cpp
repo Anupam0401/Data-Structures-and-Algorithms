0aad0378b7e4b6b0d17befb7febae1b49cdd0ee6
Anupam <anupamk@iitbhilai.ac.in>
2022-08-09 19:18:17 +0530

    done using tabulation approach in O(5n) TC and O(n) SC

--- MySubmission/LeetCode/1220.count-vowels-permutation.cpp
+++ MySubmission/LeetCode/1220.count-vowels-permutation.cpp
@@ -10,23 +10,23 @@ class Solution
 private:
     const int MOD = 1e9 + 7;
 public:
-    // int countVowelPermutation(int n)
-    // {
-    //     const int MOD = 1e9 + 7;
-    //     long dp[5][n + 1], ans = 0;
-    //     dp[0][1] = dp[1][1] = dp[2][1] = dp[3][1] = dp[4][1] = 1;
-    //     for (int i = 2; i <= n; i++)
-    //     {
-    //         dp[0][i] = dp[1][i - 1];
-    //         dp[1][i] = (dp[0][i - 1] + dp[2][i - 1]) % MOD;
-    //         dp[2][i] = (dp[0][i - 1] + dp[1][i - 1] + dp[3][i - 1] + dp[4][i - 1]) % MOD;
-    //         dp[3][i] = (dp[2][i - 1] + dp[4][i - 1]) % MOD;
-    //         dp[4][i] = dp[0][i - 1];
-    //     }
-    //     for (int i = 0; i < 5; i++)
-    //         ans = (ans + dp[i][n]) % MOD;
-    //     return ans;
-    // }
+    // tabulation bottom up iterative approach
+    int countVowelPermutation(int n)
+    {
+        long dp[5][n + 1], ans = 0;
+        dp[0][1] = dp[1][1] = dp[2][1] = dp[3][1] = dp[4][1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            dp[0][i] = dp[1][i - 1];
+            dp[1][i] = (dp[0][i - 1] + dp[2][i - 1]) % MOD;
+            dp[2][i] = (dp[0][i - 1] + dp[1][i - 1] + dp[3][i - 1] + dp[4][i - 1]) % MOD;
+            dp[3][i] = (dp[2][i - 1] + dp[4][i - 1]) % MOD;
+            dp[4][i] = dp[0][i - 1];
+        }
+        for (int i = 0; i < 5; i++)
+            ans = (ans + dp[i][n]) % MOD;
+        return ans;
+    }
 
     //recursive approach -- TLE
     int recurse(int n,unordered_map<char,vector<char>>& mappings,char start){
@@ -37,6 +37,8 @@ public:
         }
         return ans;
     }
+
+    //memoization top down recusive approach
     int recurseMemo(int n,unordered_map<char,vector<char>>& mappings,char start,unordered_map<char,vector<int>>& dp){
         if(n==0)    return 1;
         int ans = 0;
@@ -46,18 +48,18 @@ public:
         }
         return dp[start][n] = ans;
     }
-    int countVowelPermutation(int n)
-    {
-        unordered_map<char,vector<char>> mappings{{'s', {'a', 'e', 'i', 'o', 'u'}},
-                                            {'a', {'e'}},
-                                            {'e', {'a', 'i'}},
-                                            {'i', {'a', 'e', 'o', 'u'}},
-                                            {'o', {'i', 'u'}},
-                                            {'u', {'a'}}};
-        unordered_map<char,vector<int>> dp;
-        dp['s'] = dp['a'] = dp['e'] = dp['i'] = dp['o'] = dp['u'] = vector<int>(n+1,-1);
-        return recurseMemo(n,mappings,'s',dp);
-    }
+    // int countVowelPermutation(int n)
+    // {
+    //     unordered_map<char,vector<char>> mappings{{'s', {'a', 'e', 'i', 'o', 'u'}},
+    //                                         {'a', {'e'}},
+    //                                         {'e', {'a', 'i'}},
+    //                                         {'i', {'a', 'e', 'o', 'u'}},
+    //                                         {'o', {'i', 'u'}},
+    //                                         {'u', {'a'}}};
+    //     unordered_map<char,vector<int>> dp;
+    //     dp['s'] = dp['a'] = dp['e'] = dp['i'] = dp['o'] = dp['u'] = vector<int>(n+1,-1);
+    //     return recurseMemo(n,mappings,'s',dp);
+    // }
 };
 // @lc code=end
 
