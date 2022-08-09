07bc079599b79d841695f8b57df5ef24522ede68
Anupam <anupamk@iitbhilai.ac.in>
2022-08-09 11:50:02 +0530

    done using tabulation in  O(l1*l2) TC and SC

--- MySubmission/LeetCode/1143.longest-common-subsequence.cpp
+++ MySubmission/LeetCode/1143.longest-common-subsequence.cpp
@@ -25,8 +25,17 @@ public:
         int l2 = b.length();
         if (l1 == 0 || l2 == 0)
             return 0;
-        vector<vector<int>> dp(l1, vector<int>(l2, -1));
-        return recursMemo(l1 - 1, l2 - 1, a, b, dp);
+        // vector<vector<int>> dp(l1, vector<int>(l2, -1));
+        // return recursMemo(l1 - 1, l2 - 1, a, b, dp);
+
+        vector<vector<int>> dp(l1+1,vector<int>(l2+1,0));
+        for(int i=1;i<=l1;i++){
+            for(int j=1;j<=l2;j++){
+                if(a[i-1]==b[j-1])  dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[l1][l2];
     }
 };
 // @lc code=end
