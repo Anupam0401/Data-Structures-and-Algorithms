04cf5f22f100bfc51cd1deb4f4084ab8d054bce3
Anupam <anupamk@iitbhilai.ac.in>
2022-08-09 11:41:01 +0530

    memoized the recursive approach to make it O(l1*l2) TC

--- MySubmission/LeetCode/1143.longest-common-subsequence.cpp
+++ MySubmission/LeetCode/1143.longest-common-subsequence.cpp
@@ -5,19 +5,28 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int recursive(int ind1,int ind2, string a, string b){
-        if(ind1<0 || ind2<0)    return 0;
-        if(a[ind1]==b[ind2])    return 1+recursive(ind1-1,ind2-1,a,b);
-        else    return max(recursive(ind1-1,ind2,a,b),recursive(ind1,ind2-1,a,b));
+    int recursMemo(int ind1, int ind2, string &a, string &b, vector<vector<int>> &dp)
+    {
+        if (ind1 < 0 || ind2 < 0)
+            return 0;
+        if (dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
+        if (a[ind1] == b[ind2])
+            return dp[ind1][ind2] = 1 + recursMemo(ind1 - 1, ind2 - 1, a, b, dp);
+        else
+            return dp[ind1][ind2] = max(recursMemo(ind1 - 1, ind2, a, b, dp), recursMemo(ind1, ind2 - 1, a, b, dp));
     }
-    int longestCommonSubsequence(string a, string b) {
+    int longestCommonSubsequence(string a, string b)
+    {
         int l1 = a.length();
         int l2 = b.length();
-        if(l1==0 || l2==0)  return 0;
-        return recursive(l1-1,l2-1,a,b);
+        if (l1 == 0 || l2 == 0)
+            return 0;
+        vector<vector<int>> dp(l1, vector<int>(l2, -1));
+        return recursMemo(l1 - 1, l2 - 1, a, b, dp);
     }
 };
 // @lc code=end
-
