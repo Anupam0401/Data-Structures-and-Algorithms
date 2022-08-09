7df7918b7975d4b8597a7f1fd39972d3f08aa2b9
Anupam <anupamk@iitbhilai.ac.in>
2022-08-09 08:17:23 +0530

    done uisng DP -- tabulation approach in O(n)

---
+++ MySubmission/LeetCode/1220.count-vowels-permutation.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1220 lang=cpp
+ *
+ * [1220] Count Vowels Permutation
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countVowelPermutation(int n) {
+        const int MOD = 1e9 + 7;
+        long dp[5][n+1], ans = 0;
+        dp[0][1] = dp[1][1] = dp[2][1] = dp[3][1] = dp[4][1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[0][i] =  dp[1][i-1];
+            dp[1][i] = (dp[0][i-1] + dp[2][i-1]) % MOD;
+            dp[2][i] = (dp[0][i-1] + dp[1][i-1] + dp[3][i-1] + dp[4][i-1]) % MOD;
+            dp[3][i] = (dp[2][i-1] + dp[4][i-1]) % MOD;
+            dp[4][i] =  dp[0][i-1];
+        }
+        for(int i=0; i < 5; i++) 
+            ans = (ans + dp[i][n]) % MOD;
+        return ans;
+    }
+};
+// @lc code=end
+
