a65b7aad25d05bfd32a026fdf0950931de53211c
Anupam <anupamk@iitbhilai.ac.in>
2022-08-10 11:29:19 +0530

    performed iterative approach using queue in O(n) TC and SC

--- MySubmission/LeetCode/199.binary-tree-right-side-view.cpp
+++ MySubmission/LeetCode/199.binary-tree-right-side-view.cpp
@@ -29,7 +29,22 @@ public:
     vector<int> rightSideView(TreeNode* root) {
         if(!root)   return {};
         vector<int> ans;
-        getRightView(root,ans,0);
+        // getRightView(root,ans,0);
+        // return ans;
+
+        // iterative approach O(nodes) TC
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            for(int i=0;i<n;i++){
+                TreeNode* front = q.front();
+                q.pop();
+                if(i==n-1)  ans.push_back(front->val);
+                if(front->left) q.push(front->left);
+                if(front->right)    q.push(front->right);
+            }
+        }
         return ans;
     }
 };
