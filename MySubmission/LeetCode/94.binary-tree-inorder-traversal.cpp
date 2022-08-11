de33aa6d92c63f3923e70ac7670e58bb053dbe4f
Anupam <anupamk@iitbhilai.ac.in>
2022-08-11 17:35:20 +0530

    done using Morris traversal in O(n) time and O(1) space

--- MySubmission/LeetCode/94.binary-tree-inorder-traversal.cpp
+++ MySubmission/LeetCode/94.binary-tree-inorder-traversal.cpp
@@ -19,24 +19,53 @@
 class Solution
 {
 public:
-    
+    // done using iterative approach -- using stack
+    // vector<int> inorderTraversal(TreeNode *root)
+    // {
+    //     vector<int> res;
+    //     if(root==nullptr)   return res;
+    //     stack<TreeNode*> st;
+    //     TreeNode* temp = root;
+    //     while(true){
+    //         if(temp!=NULL){
+    //             st.push(temp);
+    //             temp = temp->left;
+    //         }
+    //         else{
+    //             if(st.empty())  break;
+    //             temp = st.top();
+    //             st.pop();
+    //             res.push_back(temp->val);
+    //             temp = temp->right;
+    //         }
+    //     }
+    //     return res;
+    // }
+
+    // Morris Traversal -- in O(n) time and O(1) space
     vector<int> inorderTraversal(TreeNode *root)
     {
+        TreeNode* curr = root;
         vector<int> res;
-        if(root==nullptr)   return res;
-        stack<TreeNode*> st;
-        TreeNode* temp = root;
-        while(true){
-            if(temp!=NULL){
-                st.push(temp);
-                temp = temp->left;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                res.push_back(curr->val);
+                curr = curr->right;
             }
             else{
-                if(st.empty())  break;
-                temp = st.top();
-                st.pop();
-                res.push_back(temp->val);
-                temp = temp->right;
+                TreeNode* prev = curr->left;
+                while(prev->right!=NULL && prev->right!=curr){
+                    prev = prev->right;
+                }
+                if(prev->right == NULL){
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    prev->right = NULL;
+                    res.push_back(curr->val);
+                    curr = curr->right;
+                }
             }
         }
         return res;
