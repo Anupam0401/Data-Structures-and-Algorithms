3f62febc2121a8f3a3d47595857bf6d7b85e366a
Anupam <anupamk@iitbhilai.ac.in>
2022-08-11 17:40:56 +0530

    added the desciption for the Morris traversal

---
+++ MySubmission/LeetCode/106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=106 lang=cpp
+ *
+ * [106] Construct Binary Tree from Inorder and Postorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        
+    }
+};
+// @lc code=end
+
