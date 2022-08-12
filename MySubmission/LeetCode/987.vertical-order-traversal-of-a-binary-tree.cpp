aa1c5666667ce7431cb28b3bab00256eb1bf6270
Anupam <anupamk@iitbhilai.ac.in>
2022-08-12 14:21:04 +0530

    TC: O(n*logn*logn*logn) and SC is O(n)

--- MySubmission/LeetCode/987.vertical-order-traversal-of-a-binary-tree.cpp
+++ MySubmission/LeetCode/987.vertical-order-traversal-of-a-binary-tree.cpp
@@ -16,28 +16,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>> mp;
+    vector<vector<int>> verticalTraversal(TreeNode *root)
+    {
+        map<int, map<int, multiset<int>>> mp;
         vector<vector<int>> res;
-        if(root==NULL)  return res;
-        TreeNode* curr = root;
-        queue<pair<TreeNode*,pair<int,int>>> q;
-        q.push({curr,{0,0}});
-        while(!q.empty()){
-            TreeNode* front = q.front().first;
+        if (root == NULL)
+            return res;
+        TreeNode *curr = root;
+        queue<pair<TreeNode *, pair<int, int>>> q;
+        q.push({curr, {0, 0}});
+        while (!q.empty())
+        {
+            TreeNode *front = q.front().first;
             int x = q.front().second.first;
             int y = q.front().second.second;
             q.pop();
             mp[x][y].insert(front->val);
-            if(front->left) q.push({front->left,{x-1,y+1}});
-            if(front->right) q.push({front->right,{x+1,y+1}});
+            if (front->left)
+                q.push({front->left, {x - 1, y + 1}});
+            if (front->right)
+                q.push({front->right, {x + 1, y + 1}});
         }
-        for(auto it:mp){
+        for (auto it : mp)
+        {
             vector<int> temp;
-            for(auto ele:it.second){
-                for(auto x: ele.second){
+            for (auto ele : it.second)
+            {
+                for (auto x : ele.second)
+                {
                     temp.push_back(x);
                 }
             }
@@ -47,4 +56,3 @@ public:
     }
 };
 // @lc code=end
-
