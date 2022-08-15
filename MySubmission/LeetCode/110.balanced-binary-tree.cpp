d2552e6ec33a915d342c6d601a80db5175fc3960
Anupam <anupamk@iitbhilai.ac.in>
2022-08-15 23:20:07 +0530

    done in O(n) time and O(n) auxilliary stacck space

--- MySubmission/LeetCode/110.balanced-binary-tree.cpp
+++ MySubmission/LeetCode/110.balanced-binary-tree.cpp
@@ -16,24 +16,49 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    int height(TreeNode* root){
-        if(root==NULL) return 0;
-        return max(height(root->left),height(root->right))+1;
+    int height(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+        return max(height(root->left), height(root->right)) + 1;
     }
-    bool isBalanced(TreeNode* root) {
-        if(root==NULL)  return true;
-        int lh = height(root->left);
-        int rh = height(root->right);
+    // bool isBalanced(TreeNode* root) {
+    //     if(root==NULL)  return true;
+    //     int lh = height(root->left);
+    //     int rh = height(root->right);
 
-        if(abs(lh-rh)>1)    return false;
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
+    //     if(abs(lh-rh)>1)    return false;
+    //     bool left = isBalanced(root->left);
+    //     bool right = isBalanced(root->right);
 
-        if(!left || !right) return false;
-        return true;
+    //     if(!left || !right) return false;
+    //     return true;
+    // }
+
+    // O(n) time traversal
+    int dfsHeight(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+        int lh = dfsHeight(root->left);
+        if (lh == -1)
+            return -1;
+        int rh = dfsHeight(root->right);
+        if (rh == -1)
+            return -1;
+        if (abs(lh - rh) > 1)
+            return -1;
+        return 1 + max(lh, rh);
+    }
+
+    bool isBalanced(TreeNode *root)
+    {
+        if (root == NULL)
+            return 1;
+        return dfsHeight(root) != -1;
     }
 };
 // @lc code=end
-
