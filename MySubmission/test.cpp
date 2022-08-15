a2d8ae113e60f480d526e11a3a7a886f75371971
Anupam <anupamk@iitbhilai.ac.in>
2022-08-15 19:45:08 +0530

    Codenation question

--- MySubmission/test.cpp
+++ MySubmission/test.cpp
@@ -1,18 +1,22 @@
-// Given a BST and a value k, the task is to delete the nodes having values greater than or equal to k.
+// You are given an integer array A of n distinct integers
+// In one jump, you can travel from ith index to jth index or jth to ith only if GCD(a[i],a[j])>1
 
-class Solution{
-  public:
-    /*The function should return the root of the modified tree*/
-    Node* deleteNode(Node* root, int k)
-    {
-        if(root==NULL)
-            return root;
-        if(root->data>=k || root->data==k){
-            Node* leftNode = deleteNode(root->left,k);
-            return leftNode;
+// Return 1 if for all pairs of (i,j) starting from index i can you travel to index j in some finite number of jumps else, return 0.
+
+int gcd(int a,int b){
+    if(a==0)    return b;
+    return gcd(b%a,a);
+}
+
+int Solution::solve(vector<int> &a) {
+    int n = a.size();
+    sort(a.begin(),a.end(),);
+    vector<int> dp(n,1);
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(gcd(a[i],a[j])>1)
+                dp[i] = max(dp[i],dp[j]+1);
         }
-        else if(root->data<k)
-            root->right=deleteNode(root->right,k);
-        return root;
     }
-}; 
+    return *max_element(dp.begin(),dp.end());
+}
