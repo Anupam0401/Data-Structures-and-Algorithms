ec4306433491ac351d300351731f2785d2a77e34
Anupam <anupamk@iitbhilai.ac.in>
2022-08-16 12:05:44 +0530

    done in O(n) time and O(n) recursive stack space

--- MySubmission/LeetCode/100.same-tree.cpp
+++ MySubmission/LeetCode/100.same-tree.cpp
@@ -19,7 +19,10 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL || q == NULL) return false;
+        if(p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
 // @lc code=end
