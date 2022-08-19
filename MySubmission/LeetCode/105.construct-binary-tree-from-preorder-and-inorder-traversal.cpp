4e8ab84f97ea76f62021fe6ab8c48a2cb3a091ba
Anupam <anupamk@iitbhilai.ac.in>
2022-08-19 14:22:13 +0530

    done using recursion in O(N) time and space

--- MySubmission/LeetCode/105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ MySubmission/LeetCode/105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -18,26 +18,43 @@
  */
 class Solution {
 public:
-    TreeNode* makeTree(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd)
-    {
-        if(preStart>preEnd || inStart>inEnd)
-            return nullptr;
-        int rootVal = preorder[preStart];
-        TreeNode* root = new TreeNode(rootVal);
-        int rootIndex = find(inorder.begin()+inStart, inorder.begin()+inEnd, rootVal) - inorder.begin();
-        int leftLen = rootIndex - inStart;
-        root->left = makeTree(preorder, preStart+1, preStart+leftLen, inorder, inStart, rootIndex-1);
-        root->right = makeTree(preorder, preStart+leftLen+1, preEnd, inorder, rootIndex+1, inEnd);
+    // TreeNode* makeTree(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd)
+    // {
+    //     if(preStart>preEnd || inStart>inEnd)
+    //         return nullptr;
+    //     int rootVal = preorder[preStart];
+    //     TreeNode* root = new TreeNode(rootVal);
+    //     int rootIndex = find(inorder.begin()+inStart, inorder.begin()+inEnd, rootVal) - inorder.begin();
+    //     int leftLen = rootIndex - inStart;
+    //     root->left = makeTree(preorder, preStart+1, preStart+leftLen, inorder, inStart, rootIndex-1);
+    //     root->right = makeTree(preorder, preStart+leftLen+1, preEnd, inorder, rootIndex+1, inEnd);
+    //     return root;
+    // }
+    // TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    //     int preLen = preorder.size();
+    //     int inLen = inorder.size();
+    //     if(preLen != inLen)
+    //         return NULL;
+    //     if(preLen == 0)
+    //         return NULL;
+    //     return makeTree(preorder, 0, preLen-1, inorder, 0, inLen-1);
+    // }
+
+    // done using map, TC reduced to O(n)
+    TreeNode* makeTree(int preStart,int preEnd, vector<int>& preorder, int inStart, int inEnd, vector<int>& inorder,unordered_map<int,int>& in_mp){
+        if(preStart>preEnd || inStart>inEnd)    return NULL;
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        int in_index = in_mp[preorder[preStart]];
+        int left_size = in_index - inStart;
+        root->left = makeTree(preStart+1,preStart+left_size,preorder,inStart,in_index-1,inorder,in_mp);
+        root->right = makeTree(preStart+left_size+1,preEnd,preorder,in_index+1,inEnd,inorder,in_mp);
         return root;
     }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int preLen = preorder.size();
-        int inLen = inorder.size();
-        if(preLen != inLen)
-            return NULL;
-        if(preLen == 0)
-            return NULL;
-        return makeTree(preorder, 0, preLen-1, inorder, 0, inLen-1);
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder){
+        int n = inorder.size();
+        unordered_map<int,int> in_mp;
+        for(int i=0;i<n;i++)    in_mp[inorder[i]]=i;
+        return makeTree(0,n-1,preorder,0,n-1,inorder,in_mp);
     }
 };
 // @lc code=end
