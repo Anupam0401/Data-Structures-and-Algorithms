177561fa2717ceabe8fd40725dddbedbc71e3c49
Anupam <anupamk@iitbhilai.ac.in>
2022-08-21 22:43:42 +0530

    test case for debugging

--- MySubmission/test.cpp
+++ MySubmission/test.cpp
@@ -1,22 +1,16 @@
-// You are given an integer array A of n distinct integers
-// In one jump, you can travel from ith index to jth index or jth to ith only if GCD(a[i],a[j])>1
+// You are given a binary string s. In one second, all occurrences of "01" are simultaneously replaced with "10". This process repeats until no occurrences of "01" exist.
 
-// Return 1 if for all pairs of (i,j) starting from index i can you travel to index j in some finite number of jumps else, return 0.
+// Return the number of seconds needed to complete this process.
 
-int gcd(int a,int b){
-    if(a==0)    return b;
-    return gcd(b%a,a);
-}
-
-int Solution::solve(vector<int> &a) {
-    int n = a.size();
-    sort(a.begin(),a.end(),);
-    vector<int> dp(n,1);
-    for(int i=1;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(gcd(a[i],a[j])>1)
-                dp[i] = max(dp[i],dp[j]+1);
+class Solution {
+public:
+    int secondsToRemoveOccurrences(string s) {
+        int n = s.size();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '0')
+                cnt++;
         }
+        return cnt;
     }
-    return *max_element(dp.begin(),dp.end());
-}
+};
