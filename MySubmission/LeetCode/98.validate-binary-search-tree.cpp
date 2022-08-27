d25d5d695f78facd8d0294d7c497d034dd86bb50
Anupam <anupamk@iitbhilai.ac.in>
2022-08-27 14:38:56 +0530

    done using recursion using the limit at each stage in O(n) TC and stck space

--- MySubmission/LeetCode/98.validate-binary-search-tree.cpp
+++ MySubmission/LeetCode/98.validate-binary-search-tree.cpp
@@ -18,19 +18,27 @@
  */
 class Solution {
 public:
-    void inorderTraversal(TreeNode* root, vector<int>& inorder){
-        if(root == nullptr) return;
-        inorderTraversal(root->left, inorder);
-        inorder.push_back(root->val);
-        inorderTraversal(root->right, inorder);
-    }
-    bool isValidBST(TreeNode* root) {
-        vector<int> inorder;
-        inorderTraversal(root, inorder);
-        for(int i=0;i<inorder.size()-1;i++){
-            if(inorder[i]>= inorder[i+1]) return false;
-        }
-        return true;
+    //  done using inorder traversal and validation of the the traversal formed
+    // void inorderTraversal(TreeNode* root, vector<int>& inorder){
+    //     if(root == nullptr) return;
+    //     inorderTraversal(root->left, inorder);
+    //     inorder.push_back(root->val);
+    //     inorderTraversal(root->right, inorder);
+    // }
+    // bool isValidBST(TreeNode* root) {
+    //     vector<int> inorder;
+    //     inorderTraversal(root, inorder);
+    //     for(int i=0;i<inorder.size()-1;i++){
+    //         if(inorder[i]>= inorder[i+1]) return false;
+    //     }
+    //     return true;
+    // }
+
+    bool isValidBST(TreeNode* root, long low = LONG_MIN, long high = LONG_MAX){
+        if(root == NULL)    return true;
+        if(root->val<=low || root->val>=high) return false;
+        return isValidBST(root->left,low,root->val) and isValidBST(root->right,root->val,high);
+
     }
 };
 // @lc code=end
