2b719bd8bd725f8b47bed253a9696cbc2fa01228
Anupam <anupamk@iitbhilai.ac.in>
2022-09-09 17:32:25 +0530

    formatted the code

--- OOP-and-STL/DS_implement/stack.cpp
+++ OOP-and-STL/DS_implement/stack.cpp
@@ -13,16 +13,19 @@ typedef struct Stack
     stackNode *bottom;
 } stack;
 
-void init(stack* &s){
+void init(stack *&s)
+{
     s->top = NULL;
     s->bottom = NULL;
 }
 
-void push(stack* &s, int n){
-    stackNode* newNode = (stackNode*)malloc(sizeof(stackNode));
+void push(stack *&s, int n)
+{
+    stackNode *newNode = (stackNode *)malloc(sizeof(stackNode));
     newNode->data = n;
     newNode->next = NULL;
-    if(s->bottom==NULL){
+    if (s->bottom == NULL)
+    {
         s->top = newNode;
         s->bottom = newNode;
         return;
@@ -32,38 +35,44 @@ void push(stack* &s, int n){
     return;
 }
 
-void pop(stack* &s){
-    if(s->top==NULL){
-        cout<<"\nStack is empty!"<<endl;
+void pop(stack *&s)
+{
+    if (s->top == NULL)
+    {
+        cout << "\nStack is empty!" << endl;
         return;
     }
-    cout<<"\nThe top of the stack is "<<s->top->data<<endl;
-    stackNode* temp = s->top;
+    cout << "\nThe top of the stack is " << s->top->data << endl;
+    stackNode *temp = s->top;
     s->top = s->top->next;
-    cout<<"Elements sucessfully popped from the stack!"<<endl;
+    cout << "Elements sucessfully popped from the stack!" << endl;
     free(temp);
 }
 
-void printStack(stack* s){
-    stackNode* temp = s->top;
-    cout<<"\nThe elements in the stack from top to bottom are as follows"<<endl;
-    while(temp){
-        cout<<temp->data<<" ";
+void printStack(stack *s)
+{
+    stackNode *temp = s->top;
+    cout << "\nThe elements in the stack from top to bottom are as follows" << endl;
+    while (temp)
+    {
+        cout << temp->data << " ";
         temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-int main(){
+int main()
+{
     stack *s;
     init(s);
     int n;
-    cout<<"\nEnter the number of nodes to insert in the stack: ";
-    cin>>n;
-    for(int i=0;i<n;i++){
+    cout << "\nEnter the number of nodes to insert in the stack: ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
         int temp;
-        cin>>temp;
-        push(s,temp);
+        cin >> temp;
+        push(s, temp);
     }
     printStack(s);
     pop(s);
