e237a0cea90d94a765edac198e29470dfd018598
Anupam <anupamk@iitbhilai.ac.in>
2022-09-09 15:56:35 +0530

    enqueue and deque implementation in queue

--- OOP-and-STL/DS_implement/queue.cpp
+++ OOP-and-STL/DS_implement/queue.cpp
@@ -1,24 +1,83 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-typedef struct Node{
-    int front, back;
-    int *data;
-}NODE;
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+}queueNode;
 
-void enqueue(NODE **root_ref,int n){
-    
+typedef struct Queue
+{
+    queueNode *front;
+    queueNode *rear;
+}queue;
+
+void init(queue *q)
+{
+    q->front = NULL;
+    q->rear = NULL;
 }
 
-int main(){
-    NODE *queue =(NODE*)malloc(sizeof(NODE));
-    queue->front = queue->back =-1;
-    cout<<"Enter the nnumber of nodes you want to insert in the queue: ";
-    int n,t;
-    cin>>n;
-    cout<<"\nEnter the nodes\n";
-    for(int i=0; i<n; i++){
-        cin>>t;
-        enqueue(&queue,n);
+void enqueue(queue *q, int data)
+{
+    queueNode *newNode = (queueNode *)malloc(sizeof(queueNode));
+    newNode->data = data;
+    newNode->next = NULL;
+    if (q->front == NULL)
+    {
+        q->front = newNode;
+        q->rear = newNode;
+        return;
     }
+    q->rear->next = newNode;
+    q->rear = newNode;
+}
+
+void dequeue(queue *q)
+{
+    if (q->front == NULL)
+    {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+    queueNode *temp = q->front;
+    q->front = q->front->next;
+    free(temp);
+}
+
+void print(queue *q)
+{
+    queueNode *temp = q->front;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    queue q;
+    init(&q);
+    enqueue(&q, 1);
+    enqueue(&q, 2);
+    enqueue(&q, 3);
+    enqueue(&q, 4);
+    enqueue(&q, 5);
+    print(&q);
+    dequeue(&q);
+    print(&q);
+    dequeue(&q);
+    print(&q);
+    dequeue(&q);
+    print(&q);
+    dequeue(&q);
+    print(&q);
+    dequeue(&q);
+    print(&q);
+    dequeue(&q);
+    print(&q);
+    return 0;
 }
