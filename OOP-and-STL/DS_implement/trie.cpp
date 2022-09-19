f4c92854e616446ca69993b800c7aefd4eb4f7cc
Anupam <anupamk@iitbhilai.ac.in>
2022-09-19 07:11:12 +0530

    distinct count of substrings using trie

--- OOP-and-STL/DS_implement/trie.cpp
+++ OOP-and-STL/DS_implement/trie.cpp
@@ -1,102 +1,152 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
-    Node* links[26];
+struct Node
+{
+    Node *links[26];
     bool flag = false;
 
     // check if refernce trie is present or not
-    bool containsKey(char ch){
-        return links[ch-'a']!=NULL;
+    bool containsKey(char ch)
+    {
+        return links[ch - 'a'] != NULL;
     }
 
     // put the node in the link
-    void put(char ch, Node* node){
-        links[ch-'a'] = node;
+    void put(char ch, Node *node)
+    {
+        links[ch - 'a'] = node;
     }
 
     // get the next node for traversal
-    Node* get(char ch){
-        return links[ch-'a'];
+    Node *get(char ch)
+    {
+        return links[ch - 'a'];
     }
 
     // set the end of the word
-    void setEnd(){
+    void setEnd()
+    {
         flag = true;
     }
 
     // check the end of the word
-    bool isEnd(){
+    bool isEnd()
+    {
         return flag;
     }
 };
 
-class Trie{
+class Trie
+{
 private:
-    Node* root;
+    Node *root;
+
 public:
-    Trie(){
+    Trie()
+    {
         root = new Node();
     }
 
-    void insert(string word){
-        Node* node = root;
-        for(int i=0;i<word.length();i++){
-            if(!node->containsKey(word[i]))
-                node->put(word[i],new Node());
-            
-            //move to the reference
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+                node->put(word[i], new Node());
+
+            // move to the reference
             node = node->get(word[i]);
         }
         node->setEnd();
     }
 
-    bool search(string word){
+    bool search(string word)
+    {
         Node *node = root;
-        for(int i=0;i<word.length();i++){
-            if(!node->containsKey(word[i]))
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
                 return false;
             node = node->get(word[i]);
         }
         return node->isEnd();
     }
 
-    bool startsWith(string prefix){
+    bool startsWith(string prefix)
+    {
         Node *node = root;
-        for(int i=0;i<prefix.length();i++){
-            if(!node->containsKey(prefix[i]))
+        for (int i = 0; i < prefix.length(); i++)
+        {
+            if (!node->containsKey(prefix[i]))
                 return false;
             node = node->get(prefix[i]);
         }
         return true;
     }
+
+    // misc
+    int countDistinctSubstrings(string &s)
+    {
+        Node *root = new Node();
+        int count = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++)
+        {
+            Node *node = root;
+
+            for (int j = i; j < n; j++)
+            {
+                if (!node->containsKey(s[j]))
+                {
+                    node->put(s[j], new Node());
+                    count++;
+                }
+                node = node->get(s[j]);
+            }
+        }
+        return count + 1;
+    }
 };
 
 int main()
 {
-	int n = 5;
-	vector<int> type = {1, 1, 2, 3, 2};
-	vector<string> value = {"hello", "help", "help", "hel", "hel"};
-	Trie trie;
-	for (int i = 0; i < n; i++) {
-		if (type[i] == 1) {
-			trie.insert(value[i]);
-		}
-		else if (type[i] == 2) {
-			if (trie.search(value[i])) {
-				cout << "true" << "\n";
-			}
-			else {
-				cout << "false" << "\n";
-			}
-		}
-		else {
-			if (trie.startsWith(value[i])) {
-				cout << "true" << "\n";
-			}
-			else {
-				cout << "false" << "\n";
-			}
-		}
-	}
+    int n = 5;
+    vector<int> type = {1, 1, 2, 3, 2};
+    vector<string> value = {"hello", "help", "help", "hel", "hel"};
+    Trie trie;
+    for (int i = 0; i < n; i++)
+    {
+        if (type[i] == 1)
+        {
+            trie.insert(value[i]);
+        }
+        else if (type[i] == 2)
+        {
+            if (trie.search(value[i]))
+            {
+                cout << "true"
+                     << "\n";
+            }
+            else
+            {
+                cout << "false"
+                     << "\n";
+            }
+        }
+        else
+        {
+            if (trie.startsWith(value[i]))
+            {
+                cout << "true"
+                     << "\n";
+            }
+            else
+            {
+                cout << "false"
+                     << "\n";
+            }
+        }
+    }
 }
