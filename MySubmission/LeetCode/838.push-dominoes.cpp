62d77b1afb2e01b5f7ada8c81032a45c816c42de
Anupam <anupamk@iitbhilai.ac.in>
2022-09-27 21:58:40 +0530

    done in O(1) space and O(n) time

--- MySubmission/LeetCode/838.push-dominoes.cpp
+++ MySubmission/LeetCode/838.push-dominoes.cpp
@@ -12,34 +12,37 @@ public:
         if(n<=1)    return s;
 
         int x = 0,y = 1;
-        while(x<n and y<n){
+        while(x<n and y<n){ // move the right pointer y till we find a non '.' character
             while( y<n and s[y]=='.'){
                 y++;
             }
-            if(s[x]==s[y]){
+            if(y==n and s[n-1]=='.'){ // if the right pointer goes beyond limit, we bring it back in case the element was '.'
+                y=n-1;
+            }
+            if(s[x]==s[y]){ // both are same, so we can fill the entire range with the same character
                 while(x<=y){
                     s[x++]=s[y];
                 }
             }
-            else if(s[x]=='R' and s[y]=='L'){
+            else if(s[x]=='R' and s[y]=='L'){ // both are different, so we need to fill the range with characters in between according to their nearness
                 int i = x, j = y;
                 while(i<j){
                     s[i++]=s[x];
                     s[j--]=s[y];
                 }
             }
-            else if((s[x]=='.' and s[y]=='L')||(s[x]=='R' and s[y]=='.')){
+            else if((s[x]=='.' and s[y]=='L')){ // if the left pointer is '.' and right pointer is 'L', we fill the range with 'L'
                 while(x<=y){
-                    s[x++] = s[y];
+                    s[x++] = 'L';
                 }
             }
-            x = y;
-            y++;
-        }
-        x = n-2;
-        if(s[n-1]=='.'){
-            while(x>=0 and s[x]=='.')   x--;
-            if(x>=0 and s[x]=='R')   while(x<n) s[x++]='R';
+            else if(s[x]=='R' and s[y]=='.'){ // if the left pointer is 'R' and right pointer is '.', we fill the range with 'R'
+                while(x<=y){
+                    s[x++] = 'R';
+                }
+            }
+            x = y; // move the left pointer to the right pointer
+            y++;  // move the right pointer to the next element
         }
         return s;
     }
