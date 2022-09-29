8a62b9d2887f0b1c358f1ab2c4d1ff2c01b9c906
Anupam <anupamk@iitbhilai.ac.in>
2022-09-29 11:47:11 +0530

    optimised approach to O(klog(n)) TC and O(k) SC

--- MySubmission/LeetCode/658.find-k-closest-elements.cpp
+++ MySubmission/LeetCode/658.find-k-closest-elements.cpp
@@ -10,17 +10,25 @@ public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int n = arr.size();
         if(n<=k)    return arr;
-        vector<pair<int,int>> num_diff;
-        for(int i=0;i<n;i++){
-            num_diff.push_back({arr[i],abs(arr[i]-x)});
+        priority_queue<pair<int,int>> num_diff;
+        for(int i=0;i<k;i++){
+            num_diff.push({abs(arr[i]-x),arr[i]});
         }
-        sort(num_diff.begin(),num_diff.end(),[](const pair<int,int> &a, const pair<int,int> &b){
-            if(a.second==b.second)   return a.first<b.first;
-            return a.second<b.second;
-        });
+        // sort(num_diff.begin(),num_diff.end(),[](const pair<int,int> &a, const pair<int,int> &b){
+        //     if(a.second==b.second)   return a.first<b.first;
+        //     return a.second<b.second;
+        // });
+        for(int i=k;i<n;i++){
+            int diff = num_diff.top().first;
+            if(abs(arr[i]-x)<diff){
+                num_diff.pop();
+                num_diff.push({abs(arr[i]-x),arr[i]});
+            }
+        }
         vector<int> ans;
         for(int i=0;i<k;i++){
-            ans.push_back(num_diff[i].first);
+            ans.push_back(num_diff.top().second);
+            num_diff.pop();
         }
         sort(ans.begin(),ans.end());
         return ans;
