818c0230941000e1d44885dc2047fa44fe72b7ab
Anupam <anupamk@iitbhilai.ac.in>
2022-09-30 15:57:44 +0530

    done using sieve of eratosthenes in O(log(log(n)) TC

--- NumberTheory_CP/primality_test.cpp
+++ NumberTheory_CP/primality_test.cpp
@@ -23,9 +23,24 @@ void checkPrimeII(int n){
     cout<<"PRIME"<<endl;
     return;
 }
+
+void sievePreCalculate(vector<int> &primes){
+    int n = primes.size();
+    primes[0] = primes[1]  = 0;
+    for(int i=2;i*i<=n;i++){
+        if(primes[i]){
+            for(int j=i*i;j<=n;j+=i){
+                primes[j] = 0;
+            }
+        }
+    }
+}
 int main(){
     int a;
     cin>>a;
-    checkPrimeII(a);
+    // checkPrimeII(a);
+    vector<int> primes(100001,1);
+    sievePreCalculate(primes);
+    cout<<primes[a]<<endl;
     return 0;
 }
