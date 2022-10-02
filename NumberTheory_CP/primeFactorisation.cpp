5eb0b5ef72a8b106e22cfd44db2624bf1d0d6dbc
Anupam <anupamk@iitbhilai.ac.in>
2022-10-02 15:03:40 +0530

    got the factors in O(sqrt(n)) time

--- NumberTheory_CP/primeFactorisation.cpp
+++ NumberTheory_CP/primeFactorisation.cpp
@@ -18,9 +18,25 @@ void primeFactors(int n){
     return;
 }
 
+void primeFactorsII(int n){
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            int count = 0;
+            while(n%i==0){
+                count++;
+                n = n/i;
+            }
+            cout<<i<<"^"<<count<<",";
+        }
+    }
+    if(n>1) cout<<n<<"^1";
+    cout<<endl;
+    return;
+}
+
 int main(){
     int n;
     cin>>n;
-    primeFactors(n);
+    primeFactorsII(n);
     return 0;
 }
