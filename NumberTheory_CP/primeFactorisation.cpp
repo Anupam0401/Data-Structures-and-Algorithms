b11fdda9ba821952ca540e96a87605714dacdf8f
Anupam <anupamk@iitbhilai.ac.in>
2022-10-02 14:53:59 +0530

    done in O(n) TC

---
+++ NumberTheory_CP/primeFactorisation.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define endl '\n'
+
+void primeFactors(int n){
+    for(int i=2;i<=n;i++){
+        if(n%i==0){
+            int count = 0;
+            while(n%i==0){
+                count++;
+                n = n/i;
+            }
+            cout<<i<<"^"<<count<<", ";
+        }
+    }
+    cout<<endl;
+    return;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    primeFactors(n);
+    return 0;
+}
