c09a95a318fa3486515628c9012fa16306a3b945
Anupam <anupamk@iitbhilai.ac.in>
2022-11-28 18:57:31 +0530

    done level 3 quess 1

---
+++ MySubmission/google-foobar/level3.1.py
@@ -0,0 +1,57 @@
+# With her LAMBCHOP doomsday device finished, Commander Lambda is preparing for her debut on the galactic stage - but in order to make a grand entrance,
+# she needs a grand staircase! As her personal assistant, you've been tasked with figuring out how to build the best staircase EVER.
+
+# Lambda has given you an overview of the types of bricks available, plus a budget. You can buy different amounts of the different types of bricks (for
+# example, 3 little pink bricks, or 5 blue lace bricks). Commander Lambda wants to know how many different types of staircases can be built with each
+# amount of bricks, so she can pick the one with the most options.
+
+# Each type of staircase should consist of 2 or more steps.  No two steps are allowed to be at the same height - each step must be lower than the previous
+# one. All steps must contain at least one brick. A step's height is classified as the total amount of bricks that make up that step.
+# For example, when N = 3, you have only 1 choice of how to build the staircase, with the first step having a height of 2 and the second step having a
+# height of 1: (# indicates a brick)
+
+# #
+# ##
+# 21
+
+# When N = 4, you still only have 1 staircase choice:
+
+# #
+# #
+# ##
+# 31
+
+# But when N = 5, there are two ways you can build a staircase from the given bricks. The two staircases can have heights (4, 1) or (3, 2), as shown
+# below:
+
+# #
+# #
+# #
+# ##
+# 41
+
+# #
+# ##
+# ##
+# 32
+
+# Write a function called answer(n) that takes a positive integer n and returns the number of different staircases that can be built from exactly n
+# bricks. n will always be at least 3 (so you can have a staircase at all), but no more than 200, because Commander Lambda's not made of money!
+
+
+def solution(n):
+    dp = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
+    return staircase(1, n, dp) - 1
+
+def staircase(h, l, dp):
+    if dp[h][l] != 0:
+        return dp[h][l]
+    if l == 0:
+        return 1
+    if l < h:
+        return 0
+    dp[h][l] = staircase(h + 1, l - h, dp) + staircase(h + 1, l, dp)
+    return dp[h][l]
+
+print(solution(3))
+print(solution(200))
