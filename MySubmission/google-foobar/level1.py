006ea6dafb60e0cfe0181c168ec6013c909de5a5
Anupam <anupamk@iitbhilai.ac.in>
2022-11-28 18:38:17 +0530

    level 1 solution

---
+++ MySubmission/google-foobar/level1.py
@@ -0,0 +1,16 @@
+# Due to the nature of the space station's outer paneling, all of its solar panels must be squares. Fortunately, you have one very large and flat area of solar material, a pair of industrial-strength scissors, and enough MegaCorp Solar Tape(TM) to piece together any excess panel material into more squares. For example, if you had a total area of 12 square yards of solar material, you would be able to make one 3x3 square panel (with a total area of 9). That would leave 3 square yards, so you can turn those into three 1x1 square solar panels.
+# Write a function solution(area) that takes as its input a single unit of measure representing the total area of solar panels you have (between 1 and 1000000 inclusive) and returns a list of the areas of the largest squares you could make out of those panels, starting with the largest squares first. So, following the example above, solution(12) would return [9, 1, 1, 1].
+
+def solution(area):
+    # Your code here
+    result = []
+    while area > 0:
+        temp = int(area ** 0.5)
+        result.append(temp * temp)
+        area -= temp * temp
+    return result
+    
+    
+print(solution(12))
+print(solution(15324))
+
