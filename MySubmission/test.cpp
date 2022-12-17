3749f0dbd9b87b957aef932761632f67c4752fb7
Anupam <anupamk@iitbhilai.ac.in>
2022-12-17 19:18:02 +0530

    CodeDrift Q3

--- MySubmission/test.cpp
+++ MySubmission/test.cpp
@@ -1,21 +1,32 @@
-// Given an array A of integers and two integers B and C.
-// Find out the number of wonderful subarrays.
-// An array is a wonderful subarray if the following condition hold:
-// B*sum of elements of subarray = C*length of subarray.
+// There are N clerks who sit on a long working bench. Their productivity is relative to their position in the bench.
+// Benches are labelled from 1 to N from left to right.
+// Each clerk's productivity is calculated by two factors A and B.
+// It is given that the ith clerk's productivity at position j is calaculated as :
+// {A[i] *(j-1) + B[i]*(N-j)}.
+// Task is to find the maximum productivity of the bench.
+// Return answer modulo 10^9+7.
 
-long Solution::solve(vector<int> &A, int B, int C) {
+int Solution::solve(vector<int> &A, vector<int> &B) {
     int n = A.size();
-    long long int sum = 0;
-    long long int ans = 0;
-    unordered_map<long long int, long long int> m;
-    m[0] = 1;
-    for(int i = 0; i < n; i++){
-        sum += A[i];
-        long long int x = (B*sum - C*(i+1));
-        if(m.find(x) != m.end()){
-            ans += m[x];
-        }
-        m[x]++;
+    vector<pair<int,int>> v;
+    for(int i=0;i<n;i++){
+        v.push_back({A[i],B[i]});
+    }
+    sort(v.begin(),v.end());
+    vector<int> pre(n,0);
+    vector<int> suf(n,0);
+    pre[0] = v[0].first;
+    suf[n-1] = v[n-1].second;
+    for(int i=1;i<n;i++){
+        pre[i] = pre[i-1] + v[i].first;
+    }
+    for(int i=n-2;i>=0;i--){
+        suf[i] = suf[i+1] + v[i].second;
+    }
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        int temp = pre[i] + suf[i];
+        ans = max(ans,temp);
     }
     return ans;
 }
